bcd209ad46fe282a243857ea9ba7ae53d679b6a0
Vincenzo Marconi <vincenzorm117@gmail.com>
2015-02-06 22:56:18 -0500

    Initial Commit

---
+++ COMAP_EBOLA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMAP_EBOLA
+//
+//  Created by Vincenzo on 2/6/15.
+//  Copyright (c) 2015 Vincenzo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
