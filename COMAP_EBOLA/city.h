5e38c9d36e04ef0b4612b1040af793d62838b542
Vincenzo Marconi <vincenzorm117@gmail.com>
2015-02-09 13:41:09 -0500

    prefinal?

--- COMAP_EBOLA/city.h
+++ COMAP_EBOLA/city.h
@@ -11,26 +11,19 @@
 
 #include <cstdio>
 #include <iostream>
-#include <queue>
-#include <climits>
-#include <cstdio>
-#include <deque>
 #include <cmath>
 #include <map>
 #include <fstream>
 #include <iomanip>
 
 // Simulation Parameters
-#define ALPHA   .15
-#define BETA    .5
-#define GAMMA   .2
+#define ALPHA   .1062
+#define BETA    .285     // .24 - .33
+#define GAMMA   .0708    // .177
 #define CYCLES  (365)
 #define BEGIN_SICK .001
-#define INCUBATED_NUM   1
-#define INCUBATED_BOT 1500
-#define INCUBATED_TOP 3000
-#define MOVE_MEDICINE 94
-#define MIGRATION_K 10000
+#define INCUBATED_NUM  10
+#define MIGRATION_K    10000
 
 // Display Parameters
 #define WIDTH 10
@@ -38,44 +31,58 @@
 #define PERCENTAGES false
 #define SHOW_INCUBATED false
 
+// Used to store parameters
 typedef struct {
-    unsigned long int amount;
-    int destination;
-    int cyclesLeftBeforeMoving;
-} package;
+    std::map<int, double> vaccineWeights;
+    std::map<int, double> medicineWeights;
+    unsigned int medicineDaysToWait;
+    unsigned int vaccineDaysToWait;
+    unsigned int medicineSave;
+    unsigned int vaccineSave;
+} vec;
 
 
 class city{
 public:
+    
     std::ofstream OUT;
+    
+    int name;
+    int numCities;
+    bool isPort;
+    vec params;
+    
     unsigned long int S;
     unsigned long int I;
     std::vector<unsigned long int> E;
-    std::vector<city*> neighbors;
-    std::map<int, unsigned long int> dist;
     unsigned long int R;
     unsigned long int D;
     unsigned long int N;
     unsigned long int N0;
     unsigned long int V;
-    int name;
-    int numCities;
+
+    const bool beginSick;
     
+    double mweight;
+    double vweight;
+    
+    const int myCountry;
+    
+    std::vector<city*> neighbors;
+    std::map<int, unsigned long int> dist;
     std::map<int,city*> next;
     
-    bool isPort;
-    
-    float mweight;
-    float vweight;
-
     unsigned long int *outMedicine;
     unsigned long int *inMedicine;
     
     unsigned long int *outVaccine;
     unsigned long int *inVaccine;
     
+    // Constructor and destructor
+    city(unsigned long int, std::map<int,unsigned long int>,int,vec,bool,bool,int);
     ~city();
-    city(unsigned long int, std::map<int,unsigned long int>,int);
+    
+    // Function Prototypes
     void performCycle(int);
     void display(int);
     void moveIndividuals();
@@ -85,8 +92,13 @@ public:
 };
 
 
+// Destructor
 city::~city(){
+    
+    // Close file output stream
     OUT.close();
+    
+    // Clean memory
     delete [] outMedicine;
     delete [] inMedicine;
     delete [] outVaccine;
@@ -94,25 +106,41 @@ city::~city(){
 }
 
 
-city::city(unsigned long int size, std::map<int,unsigned long int> distances, int name){
+// Constructor
+city::city(unsigned long int size, std::map<int,unsigned long int> distances, int name,
+           vec params, bool isPort, bool beginSick, int myCountry):
+    beginSick(beginSick), myCountry(myCountry){
     
+    // Open file output stream to write
     OUT.open(std::to_string(name)+".txt");
+    
+    // Keep a copy of the simulation parameters and assign
+    //      inputName and isPort condition for this city
+    this->params = params;
+    this->name = name;
+    this->isPort = isPort;
+    
+    // Initialize variables
     N = N0 = size;
-    if (name == 5){
+    
+    // Could add bool for startsInfected and that property can be manipulated through file ?????????????????????????????????????????
+    if (beginSick){
         I = BEGIN_SICK * N;
         S = (1 - BEGIN_SICK) * N;
-    }else{
+    } else{
         I = 0;
         S = N;
     }
+    
     R = D = 0;
     dist = distances;
-    this->name = name;
+    
     
     E.resize(INCUBATED_NUM);
     unsigned long int expose;
+    
     for (int i = 0; i < INCUBATED_NUM; i++) {
-        expose = BETA * BEGIN_SICK * S;
+        expose = BETA * I * S / N;
         if (expose <= S){
             S -= expose;
             E[i] = expose;
@@ -122,12 +150,17 @@ city::city(unsigned long int size, std::map<int,unsigned long int> distances, in
         }
     }
     
+    // Show initial values
     display(0);
 }
 
+
+// Create treatment shelves for city to store incoming and outgoing vaccins/treatments
 void city::createMedicineShelf(const int size){
+   
     inMedicine   = new unsigned long int[size];
     outMedicine  = new unsigned long int[size];
+    
     inVaccine    = new unsigned long int[size];
     outVaccine   = new unsigned long int[size];
     
@@ -135,6 +168,7 @@ void city::createMedicineShelf(const int size){
 }
 
 
+// Displays the state of the city to a file based on the citie's name
 void city::display(int cycle){
     
     if(PERCENTAGES){
@@ -166,25 +200,30 @@ void city::display(int cycle){
 }
 
 
+// Simulate moving people within city
 void city::performCycle(int cycle){
-    // Expose people
+    
     unsigned long int incubate = BETA * I * S / N;
     
+    //! Used to prevent underflow
     if (incubate > S){
         incubate = S;
     }
     
+    // Move incubate amount of people out of susceptible
+    // and into the incubation queue to make them exposed
     S -= incubate;
-    
     E.push_back(incubate);
     
-    // Infect People
+    // Extract person that has gone incubation period
     unsigned long int Ei = E.front();
     E.erase(E.begin());
     
+    // Calculate amount of individuals that will die
     unsigned long int deaths = ceil(ALPHA * I);
-    unsigned long int recoveries = ceil(GAMMA * I);
+    unsigned long int recoveries = GAMMA * I;
     
+    //! Used to prevent underflow
     if(I < deaths + recoveries){
         recoveries = I - deaths;
     }
@@ -202,40 +241,54 @@ void city::performCycle(int cycle){
     N -= deaths;
     
     
-    
+    // Display new values of city
     display(cycle);
 }
 
 
+// Simulate incoming and outgoing migration
 void city::moveIndividuals(){
+    
     unsigned long int dsdt,dedt,drdt,d;
     long double Mo;
     
+    // Move people in and out of neighboring cities
     for(city *p : neighbors){
+        
         // Calculate parameters to update group sizes
         d = (long)dist[p->name];
         
-        if(p->I == 0)
-            Mo = (double)(N - I) * (p->N - p->I) / (double)(d*d*MIGRATION_K);
-        else
-            Mo = (double)(N - I) * (p->N - p->I) * (double)I/p->I / (double)(d*d*MIGRATION_K);
         
-        // Update S
+        //! Used to prevent underflow
+        if(p->I == 0){
+            
+            // Calculate coefficient of migration out using (N1-I1)(N2-I2) / (d^2 * migrationConstant)
+            Mo = (double)(N - I) * (p->N - p->I) / (double)(d*d*MIGRATION_K);
+        } else {
+            
+            // Calculate coefficient of migration out using (N1-I1)(N2-I2) * (I1/I2) / (d^2 * migrationConstant)
+            Mo = (double)(N - I) * (p->N - p->I) * (double)I/p->I / (double)(d*d*MIGRATION_K);
+        }
+        
+        // Calculates the amount of change of S
         dsdt = Mo * S / (N - I);
         
-        if (dsdt > S)
+        //! Used to prevent underflow
+        if (S < dsdt)
             dsdt = S;
         
+        // Move individuals in and out of the cities from susceptible and total population
         S -= dsdt;
         N -= dsdt;
         p->S += dsdt;
         p->N += dsdt;
         
-        // Update E
+        
+        // Update E (exposed individuals)
         for (int i = 0; i < INCUBATED_NUM; i++) {
             dedt = Mo * E[i] / (N - I);
             
-            if (dedt > E[i])
+            if (E[i] < dedt)
                 dedt = E[i];
             
             E[i] -= dedt;
@@ -244,12 +297,14 @@ void city::moveIndividuals(){
             p->N += dedt;
         }
         
-        // Update R
+        // Calculates the amount of change of R
         drdt = (double)Mo * R / (N - I);
         
-        if (drdt > R)
+        //! Used to prevent underflow
+        if (R < drdt)
             drdt = R;
         
+        // Move individuals in and out of the cities from recovered and total population
         R -= drdt;
         N -= drdt;
         p->R += drdt;
@@ -260,61 +315,139 @@ void city::moveIndividuals(){
 
 }
 
-
+// Some of the vaccines go to people that appear healthy but are incubating the disease
+//      this vaccine is wasted. We calculate how much is wasted first
 void city::administerTreatment(){
-
-    if (inVaccine[name-1]){
-        if(S < inVaccine[name-1]){
-            V += S;
-            R += S;
-            inVaccine[name-1] -= S;
-            S = 0;
-        } else {
-            S -= inVaccine[name-1];
-            V += inVaccine[name-1];
-            R += inVaccine[name-1];
-            inVaccine[name-1] = 0;
-        }
-        
-    }
-
-    if (inMedicine[name-1]){
-        if(I < inMedicine[name-1]){
-            R += I;
-            inMedicine[name-1] -= I;
-            I = 0;
-        } else {
-            I -= inMedicine[name-1];
-            R += inMedicine[name-1];
-            inMedicine[name-1] = 0;
-        }
-        
-    }
+   
+    unsigned long totalExposed = 0, totalVaccinatable;
 
     
+    //Calculate total number of exposed
+    for (int i = 0; i < INCUBATED_NUM; i++){
+        totalExposed += E[i];
+    }
+    
+    // Calculate total number or people that might get vaccines
+    totalVaccinatable = totalExposed + S;
+    
+    // Throw out vaccines used on people that are already incubating
+    if (totalVaccinatable != 0){
+        
+        inVaccine[name]  -= inVaccine[name]  * totalExposed / totalVaccinatable;
+    }
+    
+    // If we are out of infected people or vaccinatable people, save some just in case and trade the rest
+    if (S == 0 || I == 0){
+        
+        // Check amount of incoming vaccine to weight of amount to save
+        //      If passes vaccines are given to most infected and susceptible neighboring city
+        if (params.vaccineSave < inVaccine[name]){
+            
+            int nodeToSendVaccine = name;
+            unsigned long int buff, max = 0;
+            
+            // Find the city with the largest amount of susceptible and infected individuals
+            for(city *c: neighbors){
+                buff = c->I*c->S;
+                if(max < buff){
+                    max = buff;
+                    nodeToSendVaccine = c->name;
+                }
+            }
+            
+            // Vaccine amount is transfered to other selected city
+            outVaccine[nodeToSendVaccine] = inVaccine[name] - params.vaccineSave;
+
+            // Keey vaccineSave amount in stock
+            inVaccine[name] = params.vaccineSave;
+        }
+    }
+    
+    //If we are out of infected people, save some medicine and trade the rest
+    if (I == 0){
+       
+        // Check amount of incoming medicine to weight of amount to save
+        //      If passes medicine are given to most infected neighboring city
+        if (params.medicineSave < inMedicine[name]){
+            
+            int nodeToSendMedicine = name;
+            unsigned long int buff, max = 0;
+            
+            // Find the city with the largest amount of infected individuals
+            for(city *c: neighbors){
+                buff = c->I;
+                if(max < buff){
+                    max = buff;
+                    nodeToSendMedicine = c->name;
+                }
+            }
+
+            // Vaccine amount is transfered to other selected city
+            outMedicine[nodeToSendMedicine] = inMedicine[name] - params.medicineSave;
+            
+            // Keey medicineSave amount in stock
+            inMedicine[name] = params.medicineSave;
+        }
+    }
+
+    // Apply vaccines if in stock in city
+    if (inVaccine[name]){
+        
+        //! Used to prevent underflow
+        if(S < inVaccine[name]){
+            V += S;
+            R += S;
+            inVaccine[name] -= S;
+            S = 0;
+        } else {
+            S -= inVaccine[name];
+            V += inVaccine[name];
+            R += inVaccine[name];
+            inVaccine[name] = 0;
+        }
+        
+    }
+
+    // Apply Medicine if in stock in city
+    if (inMedicine[name]){
+
+        //! Used to prevent underflow
+        if(I < inMedicine[name]){
+            R += I;
+            inMedicine[name] -= I;
+            I = 0;
+        } else {
+            I -= inMedicine[name];
+            R += inMedicine[name];
+            inMedicine[name] = 0;
+        }
+    }
 }
 
 
 void city::moveMedicine(){
     
+    // Move medicine to neighboring cities
     for(int i = 0; i < numCities; i++){
-        
-        if(i != name-1){
+
+        // Move medicine to it's destination city
+        if(i != name){
+            
+            // Move medicine out of city
             next[i]->inMedicine[i] += outMedicine[i];
             outMedicine[i] = 0;
-        
-        
+            
             next[i]->inVaccine[i] += outVaccine[i];
             outVaccine[i] = 0;
-        
-        
+            
+            // Turn incoming medicine into outgoing medicine
+            //      so it can be sent out next cycle
             outMedicine[i] = inMedicine[i];
             inMedicine[i] = 0;
         
             outVaccine[i] = inVaccine[i];
             inVaccine[i] = 0;
         }
-        
     }
 }
 
--- COMAP_EBOLA/main.cpp
+++ COMAP_EBOLA/main.cpp
@@ -14,51 +14,209 @@
 #include <cmath>
 #include <vector>
 #include <fstream>
-#include <cinttypes>
+#include <random>
 
-#include "province.h"
-#include "city.h"
+#include "country.h"
 
-#define INPUT_FILE "guinea.txt"
+// Input files
+#define INPUT_GUINEA        "guinea.txt"
+#define INPUT_GUINEA_BIG    "guineaBIG.txt"
+#define INPUT_LIBERIA       "liberia.txt"
+#define INPUT_SIERRA_LEONE  "sierra_leone.txt"
+#define INPUT_WEST_AFRICA   "west_africa.txt"
+
+#define INPUT_FILE INPUT_WEST_AFRICA
+
+// Output files
+#define MARKOV_RESULTS "results.txt"
 
 // Simulation Parameters
-#define RATE_OF_PRODUCTION 200
-#define PRODUCTION_LIMIT 1
+#define RATE_OF_PRODUCTION 2000
+#define MARKOV_ITERATIONS 100
 
 using namespace std;
 
 
-int main(int argc, const char * argv[]) {
+
+typedef struct {
+    double vStep;
+    double mStep;
+    int mWaitStep;
+    int vWaitStep;
+    int mSaveStep;
+    int vSaveStep;
+} vec6;
+
+
+unsigned long int simulation(vec params, string fileName){
     
+    /* Currentry is created with:
+     * Given file name
+     * Fixed rate of production
+     * The Markov model parameters 
+     */
+    country p(fileName, RATE_OF_PRODUCTION, params);
     
-    srand((unsigned int)time(0));
-    
-    ifstream in;
-    in.open(INPUT_FILE);
-    
-    province p(in, RATE_OF_PRODUCTION, PRODUCTION_LIMIT);
-    
-    
+    // Country's cities go through cycles
     for (int i = 0; i < CYCLES; i++) {
-        
         p.updateCycle();
-        
         p.moveBetweenCities();
-        
         p.moveWithinCities();
-        
-        // examine these
         p.administerTreatment();
-        
         p.moveTreatment();
-        
-        p.produceMedicine();
-        
-        p.receiveMedicine();
+        p.produceTreatment();
+        p.receiveTreatment();
     }
     
+    // Number of deaths in simulation are calculated and returned
+    unsigned long int deaths = 0;
+    for(city *c : p.cities)
+        deaths += c->D;
     
-    in.close();
+    return deaths;
+}
+
+
+vector<vec> MCMC(vec params, int iter, vec6 step, string fileName){
+    
+    unsigned long int dold, dnew, accepted;
+    double quality, randNum;
+    
+    // Used to adjust parameters and store results respectively
+    vec newparams;
+    vector<vec> histogram;
+    
+
+    // Run simulation and store death count in dold
+    dold = simulation(params, fileName);
+
+    // Initialize accepted to 0 (it keeps track of how many times the Markov Chain takes a step)
+    accepted = 0;
+    
+    // Setup distributions and random number generator for varying step sizes
+    std::uniform_real_distribution<double> qualityDistribution(0.0,1.0);
+    std::normal_distribution<double> vWeightStepDistribution(0.0, step.vStep);
+    std::normal_distribution<double> mWeightStepDistribution(0.0, step.mStep);
+    std::normal_distribution<int> mWaitStepDistribution(0.0, step.mWaitStep);
+    std::normal_distribution<int> vWaitStepDistribution(0.0, step.vWaitStep);
+    std::normal_distribution<int> mSaveStepDistribution(0.0, step.mSaveStep);
+    std::normal_distribution<int> vSaveStepDistribution(0.0, step.vSaveStep);
+    std::default_random_engine generator;
+    generator.seed((unsigned int)time(0));
+    
+    
+    for (int i = 0; i < iter; i++) {
+
+        // Make a copy of old parameters to alter with step
+        newparams = params;
+        
+        // Add a real normal randomly generator step size to the medicine weights
+        for(map<int, double>::iterator it = params.medicineWeights.begin(); it != params.medicineWeights.end(); it++){
+            it->second += mWeightStepDistribution(generator);
+        }
+        
+        // Add a real normal randomly generator step size to the vaccine weights
+        for(map<int, double>::iterator it = params.vaccineWeights.begin(); it != params.vaccineWeights.end(); it++){
+            it->second += vWeightStepDistribution(generator);
+        }
+        
+        // Add a discrete normal randomly generator step size to each of the integer parameters
+        params.medicineDaysToWait += mWaitStepDistribution(generator);
+        params.vaccineDaysToWait += vWaitStepDistribution(generator);
+        params.medicineSave += mSaveStepDistribution(generator);
+        params.vaccineSave += vSaveStepDistribution(generator);
+        
+
+        
+        // Perform simulation
+        dnew = simulation(params, fileName);
+
+        
+        // Compare old death count to new death count
+        quality = (double)dold/dnew;
+        
+        // Set randNum a real uniformly distributed number between 0 and 1
+        randNum = qualityDistribution(generator);
+        
+        // Check quality:
+        // keep newparams if death count is better than old keep newparams
+        // OR if quality is less than the real uniformly distributed random variable randNum
+        if( quality <= randNum || 1.0 <= quality){
+            
+            if(1 <= quality ){
+                
+            }
+            
+            // Set old settings to the new ones and increment the acceptance count
+            params = newparams;
+            dold = dnew;
+            accepted++;
+            
+            // Store new guesses in histogram
+            histogram.push_back(params);
+        }
+    }
+    
+    // Show acceptance rate
+    cout << "Acceptance rate: " << 100.0 * accepted / (double)iter << " %" << endl;
+    
+    
+    return histogram;
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    // Start sequence of random numbers
+    srand((unsigned int)time(0));
+    
+    // Initial parameters of estimation and step parameters for MCMC
+    vec6 steps = (vec6){0.01,0.01,1,1,100,100};
+    vec params;
+    
+    // Take in number of cities
+    ifstream IN(INPUT_FILE);
+    int numCities;
+    IN >> numCities;
+    IN.close();
+    
+    // Initial values assigned to estimation parameter
+    for (int i = 0; i < numCities; i++) {
+        params.vaccineWeights[i] = 1.0/((double)numCities);
+        params.medicineWeights[i] = 1.0/((double)numCities);
+    }
+    
+    params.vaccineDaysToWait = 4;
+    params.medicineDaysToWait = 4;
+    
+    params.medicineSave = 0;
+    params.vaccineSave = 0;
+    
+    // MCMC is ran and results retrieved
+    vector<vec> results = MCMC(params, MARKOV_ITERATIONS, steps, INPUT_FILE);
+    
+    
+    // File is created with contents of results
+    ofstream OUT(MARKOV_RESULTS);
+    
+    // Output results to output.txt file 
+    for (int i = 0; i < results.size(); i++) {
+        for (int k = 0; k < results[i].vaccineWeights.size(); k++) {
+            OUT << results[i].vaccineWeights[k] << " ";
+        }
+        
+        for (int k = 0; k < results[i].medicineWeights.size(); k++) {
+            OUT << results[i].medicineWeights[k] << " ";
+        }
+        
+        OUT << results[i].medicineDaysToWait << " ";
+        OUT << results[i].vaccineDaysToWait << " ";
+        OUT << results[i].medicineSave << " ";
+        OUT << results[i].vaccineSave << " ";
+        OUT << std::endl;
+    }
+    
+    OUT.close();
     
     return 0;
 }
